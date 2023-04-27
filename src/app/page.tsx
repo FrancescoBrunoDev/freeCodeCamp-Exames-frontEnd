465cee4fe62e6add4ec8a5a7c2f049b7dbbdd401
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2023-04-27 19:40:06 +0200

    hide last section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
           </p>
         </Link>
 
-        <Link
+        {/* <Link
           href="/examples/Pomodoroclock"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_self"
@@ -93,7 +93,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Basically a fancy Pomodoro Clock.
           </p>
-        </Link>
+        </Link> */}
 
 
       </div>
